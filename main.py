fbd3ed6ff60042ab8e60586fd907bcdd04150197
Vitaly Aduchiev <joyniter@gmail.com>
2026-08-05 16:25:21 +0300

    code: use pro-model and human- plus system messages for right style

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
 
 # автоматически прогрузить переменные
 load_dotenv()
 
-model = ChatOpenAI(model="gpt-4.1-mini")
-resp = model.invoke("What is the capital of France?")
+model = ChatOpenAI(model="gpt-5.4-pro", temperature=1, timeout=(10, 120), max_retries=0)
+resp = model.invoke([
+    SystemMessage(content="You are an experienced linguist specializing in Slavic languages"),
+    HumanMessage(content="Say 'Good day, cat!' in three languages")
+])
+print(resp.response_metadata)
 print(resp.content)
