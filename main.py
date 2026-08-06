b31b0ce9dbbf3830bfdbc4cb706af2dae67e3455
Vitaly Aduchiev <joyniter@gmail.com>
2026-08-06 09:50:41 +0300

    code: use first simple way to separate logic between prompt and decision

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from langchain_core.messages import SystemMessage, HumanMessage
 # автоматически прогрузить переменные
 load_dotenv()
 
-model = ChatOpenAI(model="gpt-5.4-pro", temperature=1, timeout=(10, 120), max_retries=0)
+task_template = "Say '{text}' in three languages"
+task = task_template.format(text=input("Input something in Russian: "))
+
+model = ChatOpenAI(model="gpt-4.1-mini", temperature=1, timeout=(10, 120), max_retries=0)
 resp = model.invoke([
     SystemMessage(content="You are an experienced linguist specializing in Slavic languages"),
-    HumanMessage(content="Say 'Good day, cat!' in three languages")
+    HumanMessage(content=task)
 ])
 print(resp.response_metadata)
 print(resp.content)
