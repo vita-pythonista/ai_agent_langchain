28893158bd50023a81e34a7985b1747937cafb11
Vitaly Aduchiev <joyniter@gmail.com>
2026-08-06 09:59:54 +0300

    code: use ChatPromptTemplate for similar decision

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
 
 # автоматически прогрузить переменные
 load_dotenv()
 
-task_template = "Say '{text}' in three languages"
-task = task_template.format(text=input("Input something in Russian: "))
+prompt_template = ChatPromptTemplate([
+    ("system", "You are an experienced linguist specializing in {lang_kind} languages"),
+    ("human", "Say '{text}' in three languages"),
+])
+
+prompt = prompt_template.format(lang_kind=input("Please enter your language kind: "),
+                                text=input("Please enter your text: "))
 
 model = ChatOpenAI(model="gpt-4.1-mini", temperature=1, timeout=(10, 120), max_retries=0)
-resp = model.invoke([
-    SystemMessage(content="You are an experienced linguist specializing in Slavic languages"),
-    HumanMessage(content=task)
-])
+resp = model.invoke(prompt)
+
 print(resp.response_metadata)
 print(resp.content)
