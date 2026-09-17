48bc3a62f4ccec2abdf3f449c1a8917c37523a17
Vitaly Aduchiev <joyniter@gmail.com>
2026-09-17 14:43:06 +0300

    refactor(main, system): use file readind, another system to form prompt

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTempla
 # автоматически прогрузить переменные
 load_dotenv()
 
+language = "arabic"
+tone = "friendly"
+
+values = {
+    "language": input(f"Please enter your language: (default:{language}) ".strip()) or language,
+    "tone": input(f"Please enter your tone: (default:{tone}) ".strip()) or tone,
+    "text": input("Please enter your text: ")
+}
+
 prompt_template = ChatPromptTemplate([
-    SystemMessagePromptTemplate.from_template_file("prompts/system.txt", input_variables=["lang_kind"]),
+    SystemMessagePromptTemplate.from_template_file("prompts/system.txt", input_variables=["language"]),
+    SystemMessagePromptTemplate.from_template("Your tone of conversations is {tone}"),
     HumanMessagePromptTemplate.from_template("Say '{text}' in three languages")
 ])
 
-prompt = prompt_template.format(lang_kind=input("Please enter your language kind: "),
-                                text=input("Please enter your text: "))
+prompt = prompt_template.format(**values)
 
 model = ChatOpenAI(model="gpt-4.1-mini", temperature=1, timeout=(10, 120), max_retries=0)
 resp = model.invoke(prompt)
